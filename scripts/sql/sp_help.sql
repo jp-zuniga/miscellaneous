fddfcc41469b3c00bce1c75a401ed77a8798d74b
jp-zuniga <jp.zuniga.dev@gmail.com>
2025-10-15 18:37:43 -0600

    save queries

---
+++ scripts/sql/sp_help.sql
@@ -0,0 +1,6 @@
+Exec SP_Help 'Sales.SalesOrderHeader';
+Exec SP_Help 'Sales.CurrencyRate';
+Exec SP_Help 'Sales.SalesOrderDetail';
+Exec SP_Help 'Production.Product';
+Exec SP_Help 'Production.ProductCategory';
+Exec SP_Help 'Production.ProductSubcategory';
